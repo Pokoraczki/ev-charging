687710150433145286d8c42dbe72af5f315b0375
Bálint <deverabalint@gmail.com>
2026-09-21 11:09:26 +0200

    Update EvChargingApplication.java

---
+++ backend-java/src/main/java/com/evcharging/EvChargingApplication.java
@@ -0,0 +1,17 @@
+package com.evcharging;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt*;
+import java.awt.event*;
+import java.awt.geom.Arc2D;
+import java.awt.geom.RoundRectangle2D;
+
+public class EvChargingApplication extends JFrame {
+  private static final Color BG_DARK = new Color(11, 14, 20);
+  private static final Color CARD_BG = new Color(21, 25, 34);
+  private static final Color CARD_BORDER = new Color(35, 42, 54);
+  private static final Color NEON_GREEN = new Color(9, 240, 116);
+  private static final Color TEXT_WHITE = new Color(243, 244, 246);
+  private static final Color TEXT_GRAY = new Color(156, 163, 175);
+}
